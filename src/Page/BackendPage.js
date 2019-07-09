e40ac6ecb241939b06edd4aa1e42398363b8591b
nuttawoot.r <nuttawoot.r@i-bitz.co.th>
2019-07-09 10:54:48 +0700

    edit backend page

--- src/Page/BackendPage.js
+++ src/Page/BackendPage.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Button, Grid } from "@material-ui/core";
+import { Button } from "@material-ui/core";
 import useReactRouter from "use-react-router";
 import LayerBackend from "../Component/Backend/LayerBackend";
 import { createStyles, makeStyles } from "@material-ui/core/styles";
@@ -11,26 +11,6 @@ const useStyles = makeStyles((theme) =>
       height: 40,
       marginTop: 100,
       borderRadius: 10
-    },
-    rot: {
-      position: "absolute",
-<<<<<<< HEAD
-      // zIndex: -1,
-      background: "linear-gradient(45deg, #F2AD2E , #F27304 ,#0C0C0C )",
-      width: 900,
-      height: 300,
-      padding: theme.spacing(3, 2),
-      marginTop: 100,
-      borderRadius: 10
-=======
-      zIndex: -1,
-      width: "100vw",
-      height: "100vh",
-      background: "linear-gradient(45deg, #F2AD2E , #F27304 ,#0C0C0C )"
-    },
-    but: {
-      zIndex: 10
->>>>>>> 266345c62c1e126b798281efbd9e445fa5b75eb2
     }
   })
 );
@@ -42,17 +22,8 @@ const BackendPage = () => {
     history.replace("/MapPage");
   };
   return (
-<<<<<<< HEAD
-    <div
-    // className={classes.rot}
-    >
-      <LayerBackend />
-=======
     <div>
-      <Grid>
-        <LayerBackend />
-      </Grid>
->>>>>>> 266345c62c1e126b798281efbd9e445fa5b75eb2
+      <LayerBackend />
       <Button color="primary" onClick={goToMap} className={classes.root}>
         GO TO MAP PAGE
       </Button>
